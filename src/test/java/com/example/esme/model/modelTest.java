435c7d164c097b63df8148f0754ab2ebad0aeccd
Robin Arellano <robinarehen@gmail.com>
2022-08-12 18:12:30 +0200

    correcciones y test de create y get rol

--- src/test/java/com/example/esme/model/modelTest.java
+++ src/test/java/com/example/esme/model/modelTest.java
@@ -1,7 +1,6 @@
 package com.example.esme.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
---
+++ src/test/java/com/example/esme/service/RolServiceTest.java
@@ -0,0 +1,89 @@
+package com.example.esme.service;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.example.esme.entity.RolEntity;
+import com.example.esme.model.RolModel;
+import com.example.esme.repository.RolRepository;
+import com.example.esme.service.Impl.RolServiceImpl;
+import com.example.esme.util.MapperUtil;
+
+public class RolServiceTest {
+
+	private RolServiceImpl rolserServiceImpl;
+	@Mock
+	private RolRepository rolRepository;
+	@Mock
+	private MapperUtil mapperUtil;
+
+	@BeforeEach
+	public void init() {
+		MockitoAnnotations.openMocks(this);
+		this.rolserServiceImpl = new RolServiceImpl(rolRepository, mapperUtil);
+	}
+
+	@Test
+	public void createRolSuccessfulTest() {
+		String rolName = "Admin";
+		RolModel rolModel = new RolModel();
+		rolModel.setNombreRol(rolName);
+
+		RolEntity rolEntity = new RolEntity();
+		Optional<RolEntity> rolEntityOptional = Optional.empty();
+
+		when(this.rolRepository.findByRoleName(rolName)).thenReturn(rolEntityOptional);
+
+		when(this.mapperUtil.mapperObject(rolModel, RolEntity.class)).thenReturn(rolEntity);
+		when(this.rolRepository.save(rolEntity)).thenReturn(rolEntity);
+		when(this.mapperUtil.mapperObject(rolEntity, RolModel.class)).thenReturn(rolModel);
+
+		RolModel response = this.rolserServiceImpl.createRol(rolModel);
+		assertNotNull(response);
+	}
+
+	@Test
+	public void createRolNotSuccessfulTest() {
+		String rolName = "Admin";
+		RolModel rolModel = new RolModel();
+		rolModel.setNombreRol(rolName);
+		RolEntity rolEntity = new RolEntity();
+		rolEntity.setRoleName(rolName);
+
+		Optional<RolEntity> rolEntityOptional = Optional.of(rolEntity);
+
+		when(this.rolRepository.findByRoleName(rolName)).thenReturn(rolEntityOptional);
+		when(this.mapperUtil.mapperObject(rolEntity, RolModel.class)).thenReturn(rolModel);
+
+		RolModel response = this.rolserServiceImpl.createRol(rolModel);
+		assertNotNull(response);
+	}
+
+	@Test
+	public void getAllRolTest() {
+		RolEntity rolEntity = new RolEntity();
+
+		RolModel rolModel = new RolModel();
+		rolModel.setId(1);
+		rolModel.setNombreRol("admin");
+		rolModel.setDescripcion("todas las opciones");
+
+		List<RolEntity> rolEntities = new ArrayList<>();
+		rolEntities.add(rolEntity);
+
+		when(this.rolRepository.findAll()).thenReturn(rolEntities);
+		when(this.mapperUtil.mapperObject(rolEntities, RolModel.class)).thenReturn(rolModel);
+
+		List<RolModel> response = this.rolserServiceImpl.getAllRol();
+		assertNotNull(response);
+	}
+}
